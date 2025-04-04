e63df32f3b2852345361aa302042493620476bb8
zumra2002 <142160711+zumra2002@users.noreply.github.com>
2025-04-04 11:14:51 +0530

    updates

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 const appointmentSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // User's name
-  date: { type: String, required: true }, // Appointment date
-  time: { type: String, required: true }, // Appointment time
-  reason: { type: String, required: true }, // Reason for appointment
-  status: { type: String, default: "Pending" }, // Appointment status
+  name: { type: String, required: true },         // User's name
+  email: { type: String, required: true },        // User's email
+  date: { type: String, required: true },         // Appointment date
+  time: { type: String, required: true },         // Appointment time
+  reason: { type: String, required: true },       // Reason for appointment
+  status: { type: String, default: "Pending" },   // Appointment status
 });
 
 export default mongoose.model("Appointment", appointmentSchema);
