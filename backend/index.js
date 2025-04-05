1c2c48b46d5e70771030a57ff0d0d83c38d875a7
zumra2002 <zumraahlam@gmail.com>
2025-04-05 09:49:15 +0530

    update frontend & backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import appointmentRoutes from "./routes/appointmentRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoute.js";
 
 dotenv.config();
 
